be28ad09dca4fc35aaaff5c94c59ff5b45c83ae0
shruti1995 <shruti.shivakumar@gmail.com>
2015-05-28 14:46:33 +0530

    added visitor signup ; structural changes

--- src/app/modelE.js
+++ src/app/modelE.js
@@ -2,11 +2,11 @@ var mongoose = require("mongoose");
 var uriUtil = require("mongodb-uri");
 
 var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
-                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };     
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
 var mongoLabUriE = "mongodb://heroku_app37234002:sur5lnj2in6matlr1s9jp7gkpj@ds031892.mongolab.com:31892/heroku_app37234002/employees";
 var mongooseUriE = uriUtil.formatMongoose(mongoLabUriE);
 // var mongooseUriE = "mongodb://localhost/employees";
-var connE = mongoose.createConnection(mongooseUriE);
+var connE = mongoose.createConnection(mongooseUriE, options);
 connE.once("open", function() {
   console.log("connection to employee db successful");
 });
--- src/app/modelV.js
+++ src/app/modelV.js
@@ -2,12 +2,12 @@ var mongoose = require("mongoose");
 var uriUtil = require("mongodb-uri");
 
 var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
-                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };     
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
 // var mongoLabUriV = "mongodb://heroku_app37234002:sur5lnj2in6matlr1s9jp7gkpj@ds031892.mongolab.com:31892/heroku_app37234002";
 var mongoLabUriV = "mongodb://heroku_app37234002:sur5lnj2in6matlr1s9jp7gkpj@ds031892.mongolab.com:31892/heroku_app37234002/visitors";
 var mongooseUriV = uriUtil.formatMongoose(mongoLabUriV);
 // var mongooseUriV = "mongodb://localhost/visitors";
-var connV = mongoose.createConnection(mongooseUriV);
+var connV = mongoose.createConnection(mongooseUriV, options);
 connV.once("open", function() {
   console.log("connection to visitor db successful");
 });
