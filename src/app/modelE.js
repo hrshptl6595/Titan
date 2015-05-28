3e257052eb126496d9b74614bbf7e5ad10c1b573
shruti1995 <shruti.shivakumar@gmail.com>
2015-05-28 14:17:07 +0530

    added visitor signup ; structural changes

--- src/app/modelE.js
+++ src/app/modelE.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 var uriUtil = require("mongodb-uri");
 
-var mongoLabUriE = "mongodb://shruti1995:shruti126@ds031892.mongolab.com:31892/heroku_app37234002/employees";
+var mongoLabUriE = "mongodb://shruti1995:shruti126@ds031892.mongolab.com:31892/heroku_app37234002";
 var mongooseUriE = uriUtil.formatMongoose(mongoLabUriE);
 // var mongooseUriE = "mongodb://localhost/employees";
 var connE = mongoose.createConnection(mongooseUriE);
@@ -15,4 +15,4 @@ EmpSchema = mongoose.Schema({
   emailE: String
 });
 
-module.exports = connE.model("employee", EmpSchema);
+module.exports = connE.model("employee", EmpSchema, "employees");
--- src/app/modelV.js
+++ src/app/modelV.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var uriUtil = require("mongodb-uri");
 
 // var mongoLabUriV = "mongodb://heroku_app37234002:sur5lnj2in6matlr1s9jp7gkpj@ds031892.mongolab.com:31892/heroku_app37234002";
-var mongoLabUriV = "mongodb://shruti1995:shruti126@ds031892.mongolab.com:31892/heroku_app37234002/visitors";
+var mongoLabUriV = "mongodb://shruti1995:shruti126@ds031892.mongolab.com:31892/heroku_app37234002";
 var mongooseUriV = uriUtil.formatMongoose(mongoLabUriV);
 // var mongooseUriV = "mongodb://localhost/visitors";
 var connV = mongoose.createConnection(mongooseUriV);
